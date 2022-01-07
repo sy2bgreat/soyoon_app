5a360f7d45eb76dc0536272e68dc33ff032c106c
이소윤 <s_lee77176@fanshaweonline.ca>
2022-01-07 11:01:01 -0800

    structure

---
+++ lib/first_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text("first page"),
+      ),
+    );
+  }
+}
