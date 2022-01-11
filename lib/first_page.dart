d2d7a7b7c8a4503f66ed1fab0a417f953d5f9a8f
이소윤 <s_lee77176@fanshaweonline.ca>
2022-01-11 11:16:07 -0800

    done

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
 
@@ -8,12 +10,47 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
+  late PdfViewerController _PdfViewerController;
+
+  @override
+  void initState() {
+    _PdfViewerController = PdfViewerController();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text("first page"),
-      ),
-    );
+        body: Stack(
+      children: [
+        SfPdfViewer.asset(
+          'assets/resume_yoon.pdf',
+          controller: _PdfViewerController,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: FloatingActionButton(
+                  child: const Icon(Icons.zoom_in),
+                  onPressed: () {
+                    _PdfViewerController.zoomLevel = 1.50;
+                  }),
+            ),
+            const SizedBox(width: 30),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: FloatingActionButton(
+                  child: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
+            ),
+          ],
+        )
+      ],
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:soyoon_app/first_page.dart';
-import 'package:soyoon_app/sec_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/link.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  GlobalKey<ScaffoldState> _ScaffoldKEy = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _ScaffoldKEy = GlobalKey<ScaffoldState>();
   void _openDrawer() {
     _ScaffoldKEy.currentState!.openDrawer();
   }
@@ -39,34 +40,114 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _ScaffoldKEy,
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                  child: Container(
+      key: _ScaffoldKEy,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Container(
                 color: Colors.amber,
+              ),
+            ),
+            ListTile(
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyApp(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Resume"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FirstPage(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      body: Stack(
+        children: [
+          Padding(
+              padding: const EdgeInsets.fromLTRB(5, 20, 0, 0),
+              child: IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black, size: 30),
+                onPressed: () => _openDrawer(),
               )),
-              ListTile(
-                title: Text("Home"),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return const SecondPage();
-                  }));
-                },
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
+                child: Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/profile.jpg'),
+                    radius: 90,
+                  ),
+                ),
+              ),
+              RichText(
+                text: const TextSpan(
+                    text: 'I am  ',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                    children: [
+                      TextSpan(
+                          text: 'Yoon,',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ))
+                    ]),
+              ),
+              RichText(
+                  text: const TextSpan(
+                      text: 'Front end Developer',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 20,
+                      ))),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Link(
+                    target: LinkTarget.blank,
+                    uri: Uri.parse(
+                        "https://www.linkedin.com/in/soyoon-lee-75ab66199"),
+                    builder: (context, followlink) => GestureDetector(
+                      onTap: followlink,
+                      child: SvgPicture.asset("assets/linkedin.svg",
+                          height: 50, width: 50),
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  Link(
+                    target: LinkTarget.self,
+                    uri: Uri.parse("https://github.com/sy2bgreat"),
+                    builder: (context, followlink) => GestureDetector(
+                        onTap: followlink,
+                        child: SvgPicture.asset("assets/github.svg",
+                            height: 50, width: 50)),
+                  )
+                ],
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                "hong3su3@gmail.com",
+                style: TextStyle(fontSize: 12),
               )
             ],
           ),
-        ),
-        body: Stack(
-          children: [
-            Padding(
-                padding: const EdgeInsets.fromLTRB(5, 20, 0, 0),
-                child: IconButton(
-                  icon: Icon(Icons.menu, color: Colors.black),
-                  onPressed: () => _openDrawer(),
-                )),
-          ],
-        ));
+        ],
+      ),
+    );
   }
 }
